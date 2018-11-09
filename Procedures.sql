2d610b991527062063a35bbfb14e2c7997d07c9e
Diego Mendez Valverde <diegomvalverde@outlook.com>
2018-11-09 00:35:23 -0600

    Added the procedure to edit a deduction

--- Procedures.sql
+++ Procedures.sql
@@ -2,8 +2,50 @@
 use WorkerForm
 go
 
-create or alter procedure dbo.wfsp_AddWorker
+-- Procedure to log in on web page
+create or alter procedure dbo.wfsp_login
+@valorDocId nvarchar(50),
+@salida as int output
+
 as
 begin
+	begin try
+		select @salida = 1
+			from Employee E
+			where @valorDocId = E.employeeDocumentId;
+			return 1;
+	end try
+	begin catch
+		select error_message();
+		set @salida=-1;
+		return -1;
+	end catch
+	
+end;
 
+go 
+
+-- Procedure edit a bonus given to a employee
+create or alter procedure dbo.wfsp_editDeduction
+@deductionId int,
+@employeeId int,
+@deductionType int,
+@amount money,
+@salida as int output
+
+as
+begin
+	begin try
+		update EmployeeDeduction 
+			set idEmployee = @employeeId, amount = @amount, idEmployeeDeductionType = @deductionType
+			where id = @deductionId;
+		set @salida = 1;
+		return 1;
+	end try
+	begin catch
+		select error_message();
+		set @salida=-1;
+		return -1;
+	end catch
+	
 end;
