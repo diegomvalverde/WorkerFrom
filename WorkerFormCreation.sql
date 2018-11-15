9338f444a9a2b7f58cd8119f62b8b60b3034456f
Kenneth Herrera <kfhv.24@gmail.com>
2018-11-14 23:04:37 -0600

    Fixed the reference of MovementJobHours.

--- WorkerFormCreation.sql
+++ WorkerFormCreation.sql
@@ -87,22 +87,21 @@ create table FormMovements
 	salary money not null
 );
 
-create table MovementJobHours
-(
-	id int primary key not null,	
-	idFormMovements int constraint FKMovementJobHours_FormMovements references FormMovements(id) not null
-);
-
 create table Presence
 (
 	id int primary key identity(1,1) not null,
 	idEmployee int constraint FKPresence_Employee references Employee(id) not null,
 	idWorkingDayType int constraint FKPresence_WorkingDayType references WorkingDayType(id) not null,
-	idMovementJobHours int constraint FKPresence_MovementJobHours references MovementJobHours(id) not null,
 	presenceDate date not null,
 	presenceStart time not null,
 	presenceEnd time not null,
 	inhability bit not null
+);
+
+create table MovementJobHours
+(
+	id int primary key not null,	
+	presenceId int constraint FKMovementJobHours_presenceId references Presence(id) not null
 );
 
 create table DeductionType
