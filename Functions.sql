fb12c4474f3ee83d1d021ea33ecf7d81617603b2
Kenneth Herrera <kfhv.24@gmail.com>
2018-11-15 13:53:51 -0600

    Fixed errors in Function wff_calculate_salary.

--- Functions.sql
+++ Functions.sql
@@ -3,7 +3,7 @@ Go
 
 Create or Alter Function [dbo].[wff_calculate_salary]
 	(
-	@employeeDocumentId int,
+	@employeeId int,
 	@start Time(7),
 	@end Time(7),
 	@workingDayType int
@@ -13,19 +13,16 @@ As Begin
 	Declare @salaryPerHour money
 	Select @salaryPerHour = hourlySalary 
 	From JobByWorkingDayType J
-	Where (Select idJob From Employee E Where employeeDocumentId = @employeeDocumentId) = J.idJob
+	Where (Select idJob From Employee E Where id = @employeeId) = J.idJob
 	and
 	J.idWorkigDayType = @workingDayType
 
 	if @start = @end Begin
 		Select @start = workingDayStart, @end = workingDayEnd
 		From WorkingDayType Where id = @workingDayType
-
-		Select @salaryPerHour = @salaryPerHour * DatePart(HOUR,@end - @start)
-		Return @salaryPerHour
 	End
 		
-	Select @salaryPerHour = @salaryPerHour * DatePart(HOUR,@end - @start)
+	Select @salaryPerHour = @salaryPerHour * DatePart(HOUR,@end ) -  DatePart(HOUR,@start)
 	Return @salaryPerHour
 
 End
