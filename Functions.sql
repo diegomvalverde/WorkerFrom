0733c6368b843843be8c894bce4e1e1152b5a239
Kenneth Herrera <kfhv.24@gmail.com>
2018-11-19 15:23:29 -0600

    Added function to calculate the extra hours to pay.

--- Functions.sql
+++ Functions.sql
@@ -26,3 +26,43 @@ As Begin
 	Return @salaryPerHour
 
 End
+go
+
+Create or Alter Function [dbo].[wff_calculate_extraHoursPayment]
+	(
+	@employeeId int,
+	@start Time(7),
+	@end Time(7),
+	@workingDayType int
+	)
+Returns money
+As Begin
+	Declare @salaryPerHour money
+	Declare @wdStart Time(7)
+	Declare @wdEnd Time(7)
+	Declare @hoursToPay int
+	Select @hoursToPay = 0
+	Select @salaryPerHour = hourlySalary 
+	From JobByWorkingDayType J
+	Where (Select idJob From Employee E Where id = @employeeId) = J.idJob
+	and
+	J.idWorkigDayType = @workingDayType
+
+	if @start = @end Begin
+		return 0
+	End
+		
+	Select @wdStart = workingDayStart, @wdEnd = workingDayEnd
+	From WorkingDayType Where id = @workingDayType
+
+	Select @salaryPerHour = @salaryPerHour * DatePart(HOUR,@end) -  DatePart(HOUR,@start)
+	If (@start < @wdStart) Begin
+		Select @hoursToPay = @hoursToPay + (DatePart(HOUR,@wdStart) - DatePart(HOUR,@Start))
+	End
+	If (@wdEnd < @end) Begin
+		Select @hoursToPay = @hoursToPay + (DatePart(HOUR,@end) - DatePart(HOUR,@wdEnd))
+	End
+	
+	Return @salaryPerHour * @hoursToPay
+End
+go
