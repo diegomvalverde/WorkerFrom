94bfa043f713e3b3d3c27baf16835c614897a16a
Diego Mendez Valverde <diegomvalverde@outlook.com>
2018-11-23 08:14:25 -0600

    Added all the web pages and almost all the functions on php to connect db with web pages

--- Web/conexion.php
+++ Web/conexion.php
@@ -1,244 +1,265 @@
 <?php
-header('Access-Control-Allow-Origin: *'); // Without this the htmls doesn't work
+//header('Access-Control-Allow-Origin: *'); // Sin esto mueren los html xD
+// Check if the form is submitted chequear si el post fue hecho
 
-try
+// Asking for function id and if connection to sql is open
+//echo estados("1702444511");
+
+// log in
+if(isset($_POST["valorDocID"]) and $_POST["valorDocID"] != "")
 {
-  $connectionOpen = false;
-  // Opening sqlserver connection
-  $servername = 'DESKTOP-LFI86EI\SQLEXPRESS';
-  $conectionInfo = array("Database"=>'WorkerForm', "UID"=>"user", "PWD"=>"userpass", "CharacterSet"=>"UTF-8");
-  $conn_sis = sqlsrv_connect($servername, $conectionInfo);
+    $valorDocId = $_POST['valorDocID'];
+    echo consultarUsuario($valorDocId);
+}
+// Consult deduction, bonus and value per hour
+if((isset($_POST["valorDocIDQuery"])) and $_POST["valorDocIDQuery"] != "")
+{
+    $valorDocId = $_POST['valorDocIDQuery'];
+    echo $valorDocId;
+//    echo estados($userId);
+}
+// edit dedution
+if((isset($_POST["idDeduction"])) and $_POST["idDeduction"] != "" and (isset($_POST["deductionAmount"])) and $_POST["deductionAmount"] != "")
+{
+    // Editar deducción
+    $idDeduction = $_POST['idDeduction'];
+    $deductionAmount = $_POST['deductionAmount'];
+    echo $idDeduction + $deductionAmount;
+//    echo estados($userId);
+}
+// ediat bonus
+if((isset($_POST["idBonus"])) and $_POST["idBonus"] != "" and (isset($_POST["bonusAmount"])) and $_POST["bonusAmount"] != "")
+{
+    // Editar deducción
+    $idBonus = $_POST['idBonus'];
+    $bonusAmount = $_POST['bonusAmount'];
+    echo $idBonus + $bonusAmount;
+//    echo estados($userId);
+}
+// edit el valor por hora de un empleado
+if((isset($_POST["idEmployeeValue"])) and $_POST["idEmployeeValue"] != "" and (isset($_POST["valueEmployee"])) and $_POST["valueEmployee"] != "")
+{
+    // Editar deducción
+    $idEmployeeValue = $_POST['idEmployeeValue'];
+    $valueEmployee = $_POST['valueEmployee'];
+    echo $idEmployeeValue + $valueEmployee;
+//    echo estados($userId);
+}
+//if((isset($_POST["idEmployeeValue"])) and $_POST["idEmployeeValue"] != "" and (isset($_POST["valueEmployee"])) and $_POST["valueEmployee"] != "")
+//{
+//    // Editar deducción
+//    $idEmployeeValue = $_POST['idEmployeeValue'];
+//    $valueEmployee = $_POST['valueEmployee'];
+//    echo $i
+////    echo estados($userId);
+//}
+if(isset($_POST["idemployeeJob"]) and $_POST["idemployeeJob"] != "")
+{
+    $idEmployee = $_POST['idemployeeJob'];
+    echo $idEmployee;
+//    echo consultarUsuario($valorDocId);
+}
+//else
+//{
+//    echo -1;
+//}
+
+// Funcion para cargar los ultimos estados de cuenta de  una cuenta dada
+function estados($param1)
+{
+    $servername = 'DESKTOP-LFI86EI\SQLEXPRESS';
+    $conectionInfo = array("Database"=>"WorkerForm", "UID"=>"user", "PWD"=>"password", "CharacterSet"=>"UTF-8");
+    $conn_sis = sqlsrv_connect($servername, $conectionInfo);
+
   if($conn_sis)
   {
-    $connectionOpen = true;
+//     return "Coneccion exitosa";
   }
   else
   {
       die(print_r(sqlsrv_errors(), true));
   }
-}
-catch(Exception $e)
-{
-  $connectionOpen = false;
-}
-// Asking for function id and if connection to sql is open
-if(isset($_POST['functionID']) and $connectionOpen)
-{
-  $function = $_POST['functionID'];
 
-  if($function == 1)
+  $output="";
+  $sql = "{call wfsp_generalQuery(?,?)}";
+  $params = array
+  (
+  array($param1,SQLSRV_PARAM_IN),
+  array(&$output, SQLSRV_PARAM_INOUT)
+  );
+
+  $stmt = sqlsrv_query($conn_sis, $sql, $params);
+
+
+  if( $stmt === false )
   {
-    $valorDocID = $_POST['valorDocID'];
-    login();
+//   echo "Error in executing statement 3.\n";
+  die( print_r( sqlsrv_errors(), true));
   }
-  else if($function == 2)
+
+//  sqlsrv_next_result($stmt);
+  sqlsrv_free_stmt($stmt);
+  sqlsrv_close($conn_sis);
+  return $output;
+}
+
+// Function to edit a deduction
+function editDeduction($param1, $param2)
+{
+    $servername = 'DESKTOP-LFI86EI\SQLEXPRESS';
+    $conectionInfo = array("Database"=>"WorkerForm", "UID"=>"user", "PWD"=>"password", "CharacterSet"=>"UTF-8");
+    $conn_sis = sqlsrv_connect($servername, $conectionInfo);
+
+    if($conn_sis)
+    {
+//     return "Coneccion exitosa";
+    }
+    else
+    {
+        die(print_r(sqlsrv_errors(), true));
+    }
+
+    $output="";
+    $sql = "{call wfsp_generalQuery(?,?)}";
+    $params = array
+    (
+        array($param1,SQLSRV_PARAM_IN),
+        array($param2,SQLSRV_PARAM_IN),
+        array(&$output, SQLSRV_PARAM_INOUT)
+    );
+
+    $stmt = sqlsrv_query($conn_sis, $sql, $params);
+
+
+    if( $stmt === false )
+    {
+//   echo "Error in executing statement 3.\n";
+        die( print_r( sqlsrv_errors(), true));
+    }
+
+//  sqlsrv_next_result($stmt);
+    sqlsrv_free_stmt($stmt);
+    sqlsrv_close($conn_sis);
+    return $output;
+}
+
+// Function to edit a bonus
+function editBonus($param1, $param2)
+{
+    $servername = 'DESKTOP-LFI86EI\SQLEXPRESS';
+    $conectionInfo = array("Database"=>"WorkerForm", "UID"=>"user", "PWD"=>"password", "CharacterSet"=>"UTF-8");
+    $conn_sis = sqlsrv_connect($servername, $conectionInfo);
+
+    if($conn_sis)
+    {
+//     return "Coneccion exitosa";
+    }
+    else
+    {
+        die(print_r(sqlsrv_errors(), true));
+    }
+
+    $output="";
+    $sql = "{call wfsp_generalQuery(?,?)}";
+    $params = array
+    (
+        array($param1,SQLSRV_PARAM_IN),
+        array($param2,SQLSRV_PARAM_IN),
+        array(&$output, SQLSRV_PARAM_INOUT)
+    );
+
+    $stmt = sqlsrv_query($conn_sis, $sql, $params);
+
+
+    if( $stmt === false )
+    {
+//   echo "Error in executing statement 3.\n";
+        die( print_r( sqlsrv_errors(), true));
+    }
+
+//  sqlsrv_next_result($stmt);
+    sqlsrv_free_stmt($stmt);
+    sqlsrv_close($conn_sis);
+    return $output;
+}
+
+// Function to edit the employee value per hour
+function editValue($param1, $param2)
+{
+    $servername = 'DESKTOP-LFI86EI\SQLEXPRESS';
+    $conectionInfo = array("Database"=>"WorkerForm", "UID"=>"user", "PWD"=>"password", "CharacterSet"=>"UTF-8");
+    $conn_sis = sqlsrv_connect($servername, $conectionInfo);
+
+    if($conn_sis)
+    {
+//     return "Coneccion exitosa";
+    }
+    else
+    {
+        die(print_r(sqlsrv_errors(), true));
+    }
+
+    $output="";
+    $sql = "{call wfsp_generalQuery(?,?)}";
+    $params = array
+    (
+        array($param1,SQLSRV_PARAM_IN),
+        array($param2,SQLSRV_PARAM_IN),
+        array(&$output, SQLSRV_PARAM_INOUT)
+    );
+
+    $stmt = sqlsrv_query($conn_sis, $sql, $params);
+
+
+    if( $stmt === false )
+    {
+//   echo "Error in executing statement 3.\n";
+        die( print_r( sqlsrv_errors(), true));
+    }
+
+//  sqlsrv_next_result($stmt);
+    sqlsrv_free_stmt($stmt);
+    sqlsrv_close($conn_sis);
+    return $output;
+}
+
+
+// Funcion para consultar clientes en la base de datos.
+function consultarUsuario($param1)
+{
+  $servername = 'DESKTOP-LFI86EI\SQLEXPRESS';
+  $conectionInfo = array("Database"=>"WorkerForm", "UID"=>"user", "PWD"=>"password", "CharacterSet"=>"UTF-8");
+  $conn_sis = sqlsrv_connect($servername, $conectionInfo);
+
+  if($conn_sis)
   {
-    $valorDocID = $_POST['valorDocID'];
-    echo $valorDocID;
-  }
-  else if($function == 3)
-  {
-    $valorDocID = $_POST['valorDocID'];
-    echo $valorDocID;
-  }
-  else if($function == 4)
-  {
-    $valorDocID = $_POST['valorDocID'];
-    echo $valorDocID;
+    // echo "Conexion exitosa";
   }
   else
   {
-    echo 0;
+      die(print_r(sqlsrv_errors(), true));
   }
-}
-else if(!$connectionOpen)
-{
-  echo -1;
-}
 
-
-// Function to login on sqlserver
-
-function login($valorDocID)
-{
-  $output = 0;
+  $outSeq=0;
   $sql = "{call wfsp_login(?,?)}";
   $params = array
   (
-  array($valorDocID,SQLSRV_PARAM_IN),
+  array($param1,SQLSRV_PARAM_IN),
   array(&$outSeq, SQLSRV_PARAM_INOUT)
   );
 
-  // Sending parameters to sql (stored procedure)
   $stmt = sqlsrv_query($conn_sis, $sql, $params) or die(print_r(sqlsrv_errors(),true));
+
   if( $stmt === false )
   {
+  // echo "Error in executing statement 3.\n";
   die( print_r( sqlsrv_errors(), true));
   }
 
   sqlsrv_next_result($stmt);
   sqlsrv_close($conn_sis);
   return $outSeq;
+
 }
-
-
-//
-// // Funcion para cargar los ultimos estados de cuenta de  una cuenta dada
-// function estadosCuenta($param1)
-// {
-//   $servername = 'DESKTOP-LFI86EI\SQLEXPRESS';
-//   $conectionInfo = array("Database"=>'BankAccount', "UID"=>"user", "PWD"=>"userpass", "CharacterSet"=>"UTF-8");
-//   $conn_sis = sqlsrv_connect($servername, $conectionInfo);
-//
-//   if($conn_sis)
-//   {
-//     // echo "Coneccion exitosa";
-//   }
-//   else
-//   {
-//       die(print_r(sqlsrv_errors(), true));
-//   }
-//
-//   $outSeq="";
-//   $sql = "{call casp_estadocuenta(?,?)}";
-//   $params = array
-//   (
-//   array($param1,SQLSRV_PARAM_IN),
-//   array(&$outSeq, SQLSRV_PARAM_INOUT)
-//   );
-//
-//   $stmt = sqlsrv_query($conn_sis, $sql, $params) or die(print_r(sqlsrv_errors(),true));
-//
-//
-//   if( $stmt === false )
-//   {
-//   // echo "Error in executing statement 3.\n";
-//   die( print_r( sqlsrv_errors(), true));
-//   }
-//
-//   sqlsrv_next_result($stmt);
-//   sqlsrv_close($conn_sis);
-//   return $outSeq;
-// }
-//
-//
-//
-// // Funcion para consultar clientes en la base de datos.
-// function consultarUsuario($param1, $param2)
-// {
-//   $servername = 'DESKTOP-LFI86EI\SQLEXPRESS';
-//   $conectionInfo = array("Database"=>'BankAccount', "UID"=>"user", "PWD"=>"userpass", "CharacterSet"=>"UTF-8");
-//   $conn_sis = sqlsrv_connect($servername, $conectionInfo);
-//
-//   if($conn_sis)
-//   {
-//     // echo "Conexion exitosa";
-//   }
-//   else
-//   {
-//       die(print_r(sqlsrv_errors(), true));
-//   }
-//
-//   $outSeq=-1;
-//   $sql = "{call casp_consultausuario(?,?,?)}";
-//   $params = array
-//   (
-//   array($param1,SQLSRV_PARAM_IN),
-//   array($param2, SQLSRV_PARAM_IN),
-//   array(&$outSeq, SQLSRV_PARAM_INOUT)
-//   );
-//
-//   $stmt = sqlsrv_query($conn_sis, $sql, $params) or die(print_r(sqlsrv_errors(),true));
-//
-//   if( $stmt === false )
-//   {
-//   // echo "Error in executing statement 3.\n";
-//   die( print_r( sqlsrv_errors(), true));
-//   }
-//
-//   sqlsrv_next_result($stmt);
-//   sqlsrv_close($conn_sis);
-//   return $outSeq;
-//
-// }
-//
-// function agregarMovimiento($param1, $param2, $param3)
-// {
-//   $servername = 'DESKTOP-LFI86EI\SQLEXPRESS';
-//   $conectionInfo = array("Database"=>'BankAccount', "UID"=>"user", "PWD"=>"userpass", "CharacterSet"=>"UTF-8");
-//   $conn_sis = sqlsrv_connect($servername, $conectionInfo);
-//
-//   if($conn_sis)
-//   {
-//     // echo "Coneccion exitosa";
-//   }
-//   else
-//   {
-//       die(print_r(sqlsrv_errors(), true));
-//   }
-//
-//   $outSeq = -1;
-//   $sql = "{call casp_movimiento(?,?,?,?)}";
-//   $params = array
-//   (
-//   array($param1,SQLSRV_PARAM_IN),
-//   array($param2, SQLSRV_PARAM_IN),
-//   array($param3, SQLSRV_PARAM_IN),
-//   array(&$outSeq, SQLSRV_PARAM_INOUT)
-//   );
-//
-//   $stmt = sqlsrv_query($conn_sis, $sql, $params) or die(print_r(sqlsrv_errors(),true));
-//
-//
-//   if( $stmt === false )
-//   {
-//   // echo "Error in executing statement 3.\n";
-//   die( print_r( sqlsrv_errors(), true));
-//   }
-//   sqlsrv_next_result($stmt);
-//   sqlsrv_close($conn_sis);
-//   return $outSeq;
-// }
-//
-//
-// function insertarCuenta($param1, $param2)
-// {
-//   $servername = 'DESKTOP-LFI86EI\SQLEXPRESS';
-//   $conectionInfo = array("Database"=>'BankAccount', "UID"=>"user", "PWD"=>"userpass", "CharacterSet"=>"UTF-8");
-//   $conn_sis = sqlsrv_connect($servername, $conectionInfo);
-//
-//   if($conn_sis)
-//   {
-//     // echo "Coneccion exitosa";
-//   }
-//   else
-//   {
-//       die(print_r(sqlsrv_errors(), true));
-//   }
-//
-//   $outSeq = -1;
-//   $sql = "{call casp_agregarcuenta(?,?,?)}";
-//
-//   $params = array
-//   (
-//   array($param1,SQLSRV_PARAM_IN),
-//   array($param2, SQLSRV_PARAM_IN),
-//   array(&$outSeq, SQLSRV_PARAM_INOUT)
-//   );
-//
-//   $stmt = sqlsrv_query($conn_sis, $sql, $params) or die(print_r(sqlsrv_errors(),true));
-//
-//
-//   if( $stmt === false )
-//   {
-//   // echo "Error in executing statement 3.\n";
-//   die( print_r( sqlsrv_errors(), true));
-//   }
-//
-//   sqlsrv_next_result($stmt);
-//   sqlsrv_close($conn_sis);
-//   return $outSeq;
-// }
-//
 
 ?>
